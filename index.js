64bde28070323ddb9fd70068ebc60af89a59b732
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2021-08-28 13:29:14 +0300

    format all files with prettier

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const helmet = require("helmet");
-const morgan = require("morgan");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
-  console.log("connected to mongoDB");
+	console.log('connected to mongoDB');
 });
 
 // middleware
 app.use(express.json());
 app.use(helmet());
-app.use(morgan("common"));
+app.use(morgan('common'));
 
-app.get("/", (req, res) => {
-  res.send("home page");
+app.get('/', (req, res) => {
+	res.send('home page');
 });
 
 app.listen(3333);
