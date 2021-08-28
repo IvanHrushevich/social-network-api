5a606bdb38507ebf09b16933089b4eee8eb082cc
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2021-08-28 14:41:16 +0300

    implement User model and register endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,20 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-	res.send('auth page');
+const User = require('../models/user');
+
+router.post('/register', async (req, res) => {
+	const newUser = User({
+		username: req.body.username,
+		email: req.body.email,
+		password: req.body.password,
+	});
+
+	try {
+		const user = await newUser.save();
+		res.status(200).json(user);
+	} catch (err) {
+		console.log(`err`, err);
+	}
 });
 
 module.exports = router;
