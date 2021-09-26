2f821ab9a094a0ea062d36c6c9e7079b3b8658d4
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2021-09-26 19:36:11 +0300

    implement user edit endpoint

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
+
+const User = require('../models/user');
 
 router.get('/', (req, res) => {
 	res.send('users page');
+});
+
+router.put('/:id', async (req, res) => {
+	if (req.body.userId === req.params.id || req.user.isAdmin) {
+		if (req.body.password) {
+			try {
+				const salt = await bcrypt.genSalt(10);
+				req.body.password = await bcrypt.hash(req.body.password, salt);
+			} catch (err) {
+				return res.status(500).json(err);
+			}
+		}
+
+		try {
+			await User.findByIdAndUpdate(req.params.id, {
+				$set: req.body,
+			});
+
+			return res.status(200).json('account has been updated');
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	}
+
+	res.status(403).json('you can update only your account');
 });
 
 module.exports = router;
