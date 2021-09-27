29b0cae5bd9316ebd15ab0e41018c3349512ab91
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2021-09-27 21:19:11 +0300

    implement user get endpoint and "follow user" endpoint

--- routes/users.js
+++ routes/users.js
@@ -45,4 +45,38 @@ router.delete('/:id', async (req, res) => {
 	res.status(403).json('you can delete only your account');
 });
 
+router.get('/:id', async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		const { password, updatedAt, ...other } = user._doc;
+		res.status(200).json(other);
+	} catch (err) {
+		return res.status(500).json(err);
+	}
+});
+
+router.put('/:id/follow', async (req, res) => {
+	const userId = req.body.userId;
+	const userToFollowId = req.params.id;
+
+	if (userId !== userToFollowId) {
+		try {
+			const userToFollow = await User.findById(userToFollowId);
+			const user = await User.findById(userId);
+
+			if (!userToFollow.followers.includes(userId)) {
+				await userToFollow.updateOne({ $push: { followers: userId } });
+				await user.updateOne({ $push: { followings: userToFollowId } });
+				res.status(200).json('user has been followed');
+			} else {
+				res.status(400).json('you already follow this user');
+			}
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		res.status(403).json('you can delete only your account');
+	}
+});
+
 module.exports = router;
