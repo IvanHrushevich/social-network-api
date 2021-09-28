f89e47109565f268dc673a1f279b494f0dac42aa
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2021-09-28 21:22:59 +0300

    implement post api

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const morgan = require('morgan');
 
 const authRoute = require('./routes/auth');
 const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
 
 const app = express();
 
@@ -15,12 +16,12 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
 	console.log('connected to mongoDB');
 });
 
-// middleware
 app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 
 app.listen(3333);
