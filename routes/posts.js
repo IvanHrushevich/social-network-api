327bf0c87165c80fcba0863f1d7a42302b43ffff
Ivan Hrushevich <ivan.grushevich1@gmail.com>
2021-10-04 10:18:46 +0300

    implement timeline endpoint

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 router.post('/', async (req, res) => {
 	const newPost = new Post(req.body);
@@ -64,6 +65,19 @@ router.get('/:id', async (req, res) => {
 	try {
 		const post = await Post.findById(req.params.id);
 		res.status(200).json(post);
+	} catch (err) {
+		res.status(500).json(err);
+	}
+});
+
+router.get('/timeline/all', async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.body.userId);
+		const userPosts = await Post.find({ userId: currentUser._id });
+		const friendPosts = await Promise.all(
+			currentUser.followings.map(friendId => Post.find({ userId: friendId }))
+		);
+		res.status(200).json([...userPosts, ...friendPosts]);
 	} catch (err) {
 		res.status(500).json(err);
 	}
